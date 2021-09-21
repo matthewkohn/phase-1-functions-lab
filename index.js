e7214535b379cb15941867b1062e3f7696c30f06
Matthew <kohney319m@gmail.com>
2021-09-21 12:43:26 -0600

    commit tree c95aafd9fae8c95a49c7ddf707abd4b03f868c02

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const headquarters = 42;
+
+function distanceFromHqInBlocks(pickup) {
+  return Math.abs(pickup - headquarters);  
+}
+
+function distanceFromHqInFeet(pickup) {
+  let blocks = distanceFromHqInBlocks(pickup);
+  let feet = blocks * 264;
+  return feet;
+}
+
+function distanceTravelledInFeet(north, south) {
+  let feet = Math.abs(north - south) * 264;
+  return feet;
+}
+
+function calculatesFarePrice(north, south) {
+  let distance = distanceTravelledInFeet(north, south);
+  if (distance >= 2500) {
+    return "cannot travel that far";
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else if (distance < 2000 && distance > 400) {
+    distance -= 400;
+    return distance * 0.02;
+  } else if (distance <= 400) {
+    return 0;
+  }
+}
